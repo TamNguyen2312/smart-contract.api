80cef94fadd63e3da800f34e2b034e16558d24c7
TamNguyen2312 <th3m1nd05@gmail.com>
2024-11-23 06:26:06 +0700

    Update ContractTypesController

---
+++ app_source/Controllers/ContractsController.cs
@@ -0,0 +1,12 @@
+using FS.BaseAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers
+{
+    // [Route("api/[controller]")]
+    [ApiController]
+    public class ContractsController : BaseAPIController
+    {
+    }
+}
