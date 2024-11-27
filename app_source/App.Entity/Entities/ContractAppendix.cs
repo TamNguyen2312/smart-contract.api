7f0ccc47dc902e3abafdb217fbed357526645c57
TamNguyen2312 <th3m1nd05@gmail.com>
2024-11-27 16:46:23 +0700

    add ContractAppendix Table

---
+++ app_source/App.Entity/Entities/ContractAppendix.cs
@@ -0,0 +1,15 @@
+using System.Security.Principal;
+using FS.Commons.Models;
+
+namespace App.Entity.Entities;
+
+public class ContractAppendix : CommonDataModel
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTime SignedDate { get; set; }
+    public DateTime EffectiveDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public string Content { get; set; } = null!;
+    public string? FileName { get; set; }
+}
---
+++ app_source/Migrations/App/20241127094544_addContractAppendixTbl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.API.Migrations.App
+{
+    /// <inheritdoc />
+    public partial class addContractAppendixTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "App_ContractAppendices",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SignedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EffectiveDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App_ContractAppendices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "App_ContractAppendices",
+                schema: "app");
+        }
+    }
+}
